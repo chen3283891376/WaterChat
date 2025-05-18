352cb79da6b9f012f5e3e110cd72d2e13ac8284d
lrs2187 <liurongshuo2022@outlook.com>
2025-05-18 15:59:16 +0800

    💫feat: 添加 Progress

---
+++ lib/wm-ui/components/Progress.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Progress.css';
+
+export function Progress({
+    value = 60,
+    max = 100,
+    showText = true,
+    className = '',
+    ...props
+}: { value?: number; max?: number; showText?: boolean } & React.HTMLAttributes<HTMLDivElement>) {
+    const width = Math.round((value / max) * 100);
+
+    return (
+        <div className={'wm-progress ' + className}>
+            <div className="wm-progress-bar" style={{ width: `${width}%` }}>
+                {showText ? `${width}%` : null}
+            </div>
+        </div>
+    );
+}
