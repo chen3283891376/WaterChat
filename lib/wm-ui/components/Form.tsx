ca8d97b0063ab6440eb49bc1de81b89067464129
lrs2187 <liurongshuo2022@outlook.com>
2025-05-24 19:45:21 +0800

    🐛fix: 为 FormControl 引入 forwardRef

--- lib/wm-ui/components/Form.tsx
+++ lib/wm-ui/components/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 export function FormGroup({ className = '', children, ...props }: React.HTMLAttributes<HTMLDivElement>) {
     return (
@@ -16,16 +16,18 @@ export function FormLabel({ className = '', children, ...props }: React.LabelHTM
     );
 }
 
-export function FormControl({ className = '', children, ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
-    return (
-        <input
-            className={
-                'flex-1 rounded-md border border-b-2 border-black border-b-sky-500 px-1 text-black focus-visible:border-b-sky-600 focus-visible:outline dark:border-white dark:border-b-sky-500 dark:text-white ' +
-                className
-            }
-            {...props}
-        >
-            {children}
-        </input>
-    );
-}
+export const FormControl = forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+    ({ className = '', children, ...props }, ref) => {
+        return (
+            <input
+                className={
+                    'flex-1 rounded-md border border-b-2 border-black border-b-sky-500 px-1 text-black focus-visible:border-b-sky-600 focus-visible:outline dark:border-white dark:border-b-sky-500 dark:text-white ' +
+                    className
+                }
+                {...props}
+            >
+                {children}
+            </input>
+        );
+    },
+);
