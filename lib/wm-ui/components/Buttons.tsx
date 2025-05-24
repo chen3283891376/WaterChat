0721d0916106f3307cf5208f1d51942b79b4c5de
lrs2187 <liurongshuo2022@outlook.com>
2025-05-24 17:08:19 +0800

    💫feat: 添加动画

--- lib/wm-ui/components/Buttons.tsx
+++ lib/wm-ui/components/Buttons.tsx
@@ -4,7 +4,7 @@ export function Button({ className = '', children, ...props }: React.ButtonHTMLA
     return (
         <button
             className={
-                'rounded-md border border-black px-2 py-1 text-black hover:bg-emerald-100 dark:border-white dark:text-white dark:hover:bg-emerald-900 ' +
+                'rounded-md border border-black px-2 py-1 text-black transition hover:bg-emerald-100 active:bg-emerald-200 dark:border-white dark:text-white dark:hover:bg-emerald-900 dark:active:bg-emerald-800 ' +
                 className
             }
             {...props}
@@ -14,15 +14,11 @@ export function Button({ className = '', children, ...props }: React.ButtonHTMLA
     );
 }
 
-export function TransparentButton({
-    className = '',
-    children,
-    ...props
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+export function LinkButton({ className = '', children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
         <button
             className={
-                'rounded-md border-0 px-2 py-1 text-black hover:bg-slate-100 hover:text-sky-500 dark:text-white dark:hover:bg-slate-900' +
+                'rounded-md border-0 px-2 py-1 text-black transition hover:bg-slate-100 hover:text-sky-500 active:bg-slate-200 dark:text-white dark:hover:bg-slate-900 dark:active:bg-slate-800 ' +
                 className
             }
             {...props}
@@ -36,7 +32,7 @@ export function DangerousButton({ className = '', children, ...props }: React.Bu
     return (
         <button
             className={
-                'rounded-md border border-black px-2 py-1 text-black hover:bg-red-500 hover:text-white dark:border-white dark:text-white' +
+                'rounded-md border border-black px-2 py-1 text-black transition hover:bg-red-500 hover:text-white active:bg-red-600 dark:border-white dark:text-white ' +
                 className
             }
             {...props}
@@ -46,17 +42,20 @@ export function DangerousButton({ className = '', children, ...props }: React.Bu
     );
 }
 
-export function CircleButton({ radius = 20, className = '', style = {}, children, ...props }: { radius?: number } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
-    style.width = `${2 * radius}px`;
-    style.height = `${2 * radius}px`;
-
+export function CircleButton({
+    radius = 20,
+    className = '',
+    style = {},
+    children,
+    ...props
+}: { radius?: number } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
         <button
             className={
-                'rounded-full border border-black text-black hover:bg-teal-500 dark:border-white dark:text-white ' +
+                'rounded-full border border-black text-black transition hover:bg-slate-200 active:bg-slate-300 dark:border-white dark:text-white dark:hover:bg-slate-700 dark:active:bg-slate-600 ' +
                 className
             }
-            style={style}
+            style={{ ...style, width: 2 * radius, height: 2 * radius }}
             {...props}
         >
             {children}
