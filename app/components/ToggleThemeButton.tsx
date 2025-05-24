d35caf06c4bd5c02ffbdd870f89b8c0d551c98bf
lrs2187 <liurongshuo2022@outlook.com>
2025-05-24 17:09:42 +0800

    💫feat: 使用 emoji

--- app/components/ToggleThemeButton.tsx
+++ app/components/ToggleThemeButton.tsx
@@ -1,14 +1,13 @@
-import React, { type MouseEvent } from 'react';
-import {CircleButton} from '@/wm-ui';
+import React, { type MouseEvent, useState } from 'react';
+import { CircleButton } from '@/wm-ui';
 
 export default function ToggleThemeButton({ className = '', style }: React.HTMLAttributes<HTMLDivElement>) {
+    const [darkState, setDarkState] = useState(false);
+
     const toggleTheme = (event: MouseEvent) => {
         const x = event.clientX;
         const y = event.clientY;
-        const endRadius = Math.hypot(
-            Math.max(x, innerWidth - x),
-            Math.max(y, innerHeight - y)
-        );
+        const endRadius = Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y));
 
         let isDark: boolean;
 
@@ -17,13 +16,11 @@ export default function ToggleThemeButton({ className = '', style }: React.HTMLA
             isDark = root.classList.contains('dark');
             root.classList.remove(isDark ? 'dark' : 'light');
             root.classList.add(isDark ? 'light' : 'dark');
+            setDarkState(!isDark);
         });
 
         transition.ready.then(() => {
-            const clipPath = [
-                `circle(0px at ${x}px ${y}px)`,
-                `circle(${endRadius}px at ${x}px ${y}px)`,
-            ];
+            const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${endRadius}px at ${x}px ${y}px)`];
             document.documentElement.animate(
                 {
                     clipPath: isDark ? [...clipPath].reverse() : clipPath,
@@ -31,17 +28,15 @@ export default function ToggleThemeButton({ className = '', style }: React.HTMLA
                 {
                     duration: 400,
                     easing: 'ease-in',
-                    pseudoElement: isDark
-                        ? '::view-transition-old(root)'
-                        : '::view-transition-new(root)',
-                }
+                    pseudoElement: isDark ? '::view-transition-old(root)' : '::view-transition-new(root)',
+                },
             );
         });
     };
 
     return (
-        <CircleButton style={style} className={className} onClick={toggleTheme} >
-            主
+        <CircleButton style={style} className={className} onClick={toggleTheme}>
+            {darkState ? '☀️' : '🌙'}
         </CircleButton>
     );
 }
