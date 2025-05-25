5f3d3446c13cdaad2d64c60bb1bae4badc8019fa
lrs2187 <liurongshuo2022@outlook.com>
2025-05-25 22:29:06 +0800

    💫feat: 添加 nav tooltip

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,6 +12,7 @@ import type { SuccessResponse } from '~/types/base';
 import type { RoomType } from '~/types/room';
 import { getAccessToken } from '~/utils';
 import { RoomList } from '~/components/Rooms';
+import { Tooltip } from '@/wm-ui/components/Tooltip';
 
 export default function Index() {
     const tr = Translator('index', 'zh');
@@ -65,39 +66,53 @@ export default function Index() {
             </div>
             <div className="flex flex-1 gap-2">
                 <div className="flex flex-col gap-2" style={{ width: 44 }}>
-                    <LinkButton
-                        className={'py-2' + (currentPage === 1 ? ' bg-slate-200 dark:bg-slate-700' : '')}
-                        onClick={() => {
-                            setCurrentPage(1);
-                            getRooms('public');
-                        }}
-                    >
-                        <IconChat size="100%" />
-                    </LinkButton>
-                    <LinkButton
-                        className={'py-2' + (currentPage === 2 ? ' bg-slate-200 dark:bg-slate-700' : '')}
-                        onClick={() => {
-                            setCurrentPage(2);
-                            getRooms('private');
-                        }}
-                    >
-                        <IconFriend size="100%" />
-                    </LinkButton>
-                    <LinkButton className="py-2">
-                        <IconAddFriend size="100%" />
-                    </LinkButton>
-                    <LinkButton className="py-2">
-                        <IconCreateRoom size="100%" />
-                    </LinkButton>
+                    <Tooltip tooltip="群组">
+                        <LinkButton
+                            className={'py-2 hover:bg-slate-200' + (currentPage === 1 ? ' bg-slate-200 dark:bg-slate-700' : '')}
+                            onClick={() => {
+                                setCurrentPage(1);
+                                getRooms('public');
+                            }}
+                        >
+                            <IconChat size="100%" />
+                        </LinkButton>
+                    </Tooltip>
+
+                    <Tooltip tooltip="好友">
+                        <LinkButton
+                            className={'py-2 hover:bg-slate-200' + (currentPage === 2 ? ' bg-slate-200 dark:bg-slate-700' : '')}
+                            onClick={() => {
+                                setCurrentPage(2);
+                                getRooms('private');
+                            }}
+                        >
+                            <IconFriend size="100%" />
+                        </LinkButton>
+                    </Tooltip>
+
+                    <Tooltip tooltip="添加好友">
+                        <LinkButton className="py-2 hover:bg-slate-200">
+                            <IconAddFriend size="100%" />
+                        </LinkButton>
+                    </Tooltip>
+
+                    <Tooltip tooltip="创建房间">
+                        <LinkButton className="py-2 hover:bg-slate-200">
+                            <IconCreateRoom size="100%" />
+                        </LinkButton>
+                    </Tooltip>
 
                     <div className="flex-1">{/* 幽灵 div 实现中间留白 */}</div>
                     <ToggleThemeButton className="text-2xl" radius={22} />
-                    <LinkButton className="py-2">
-                        <IconSettings size="100%" />
-                    </LinkButton>
+
+                    <Tooltip className="wm-tooltip-top-right" tooltip="设置">
+                        <LinkButton className="py-2 hover:bg-slate-200">
+                            <IconSettings size="100%" />
+                        </LinkButton>
+                    </Tooltip>
                 </div>
                 <Frame className="flex flex-col bg-white dark:bg-gray-950" style={{ width: 150 }}>
-                    <h1 className="p-1 text-2xl">{currentPage === 1 ? '聊天' : '私聊'}</h1>
+                    <h1 className="p-1 text-2xl">{currentPage === 1 ? '群组' : '私聊'}</h1>
                     <hr />
                     <RoomList infos={rooms} onClick={roomId => console.log('打开房间', roomId)} />
                 </Frame>
