05a903f02a9f0b0e9cc7fe9a9f2abc54ecf617a9
lrs2187 <liurongshuo2022@outlook.com>
2025-06-06 22:25:05 +0800

    📦chore: prettier & npm update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+import { defineConfig } from 'vite';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    plugins: [
+        tailwindcss(),
+        reactRouter(),
+        tsconfigPaths(),
+    ],
     server: {
         host: '127.0.0.1',
         port: 8991,
