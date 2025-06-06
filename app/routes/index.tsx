46b9a4d39aa465aab7a5d1243c50c8b39c7b72f1
lrs2187 <liurongshuo2022@outlook.com>
2025-06-06 22:25:30 +0800

    💫feat: 简单的pwa支持

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button, Frame, LinkButton } from '@/wm-ui';
-import Translator from '~/translation/translator';
+import Translator, { type Languages } from '~/translation/translator';
 import type { Route } from './+types/index';
 import ToggleThemeButton from '~/components/ToggleThemeButton';
 import IconSettings from '~/icons/IconSettings';
@@ -13,9 +13,14 @@ import type { RoomType } from '~/types/room';
 import { getAccessToken } from '~/utils';
 import { RoomList } from '~/components/Rooms';
 import { Tooltip } from '@/wm-ui/components/Tooltip';
+import { User } from '~/api/user';
 
 export default function Index() {
-    const tr = Translator('index', 'zh');
+    const [language, setLanguage] = useState<Languages>('zh');
+    const tr = Translator('index', language);
+    const user = useRef<User>(new User('8888888888888888'));
+
+    const [isLoadedInfo, setIsLoadedInfo] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
     const [rooms, setRooms] = useState<RoomType[]>([]);
 
@@ -34,41 +39,38 @@ export default function Index() {
                 location.href = '/login';
                 return;
             }
+
+            user.current = new User(localStorage.getItem('access_token') as string);
+
+            await user.current.loadInfo();
+            setIsLoadedInfo(true);
             await getRooms('public');
         })();
     }, []);
 
     const getRooms = async (type: 'public' | 'private') => {
-        const typeMap = {
-            public: '/api/room/has',
-            private: '/api/room/private',
-        };
-
-        const response = await fetch(
-            typeMap[type] +
-                '?' +
-                new URLSearchParams({
-                    access_token: getAccessToken(),
-                }).toString(),
-        );
-
-        const responseJson: SuccessResponse<RoomType[]> = await response.json();
-        console.log(responseJson);
-        setRooms(responseJson.data);
+        if (type === 'public') {
+            setRooms(await user.current.getPublicRooms());
+        } else if (type === 'private') {
+            setRooms(await user.current.getPrivateRooms());
+        }
     };
 
     return (
         <div className="flex h-full flex-col gap-2 bg-slate-100 p-2 dark:bg-slate-800">
-            <div className="flex gap-2">
+            <div className="flex gap-2 title-bar">
+                <img src="/watermelon.png" alt="logo" style={{ height: '24px' }} />
                 <div>水瓜聊天</div>
                 <div className="flex-1">{/* 幽灵 div 实现中间留白 */}</div>
-                <div>欢迎您，******</div>
+                <div>欢迎您，{isLoadedInfo ? user.current.name : '******'}</div>
             </div>
             <div className="flex flex-1 gap-2">
                 <div className="flex flex-col gap-2" style={{ width: 44 }}>
                     <Tooltip tooltip="群组">
                         <LinkButton
-                            className={'py-2 hover:bg-slate-200' + (currentPage === 1 ? ' bg-slate-200 dark:bg-slate-700' : '')}
+                            className={
+                                'py-2 hover:bg-slate-200' + (currentPage === 1 ? ' bg-slate-200 dark:bg-slate-700' : '')
+                            }
                             onClick={() => {
                                 setCurrentPage(1);
                                 getRooms('public');
@@ -80,7 +82,9 @@ export default function Index() {
 
                     <Tooltip tooltip="好友">
                         <LinkButton
-                            className={'py-2 hover:bg-slate-200' + (currentPage === 2 ? ' bg-slate-200 dark:bg-slate-700' : '')}
+                            className={
+                                'py-2 hover:bg-slate-200' + (currentPage === 2 ? ' bg-slate-200 dark:bg-slate-700' : '')
+                            }
                             onClick={() => {
                                 setCurrentPage(2);
                                 getRooms('private');
